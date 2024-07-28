aa5780a12cabc262c8142ec43367d181d6af0266
Tetiana <shahdi007@gmail.com>
2024-07-28 18:42:16 -0400

    menu button

--- script/script.js
+++ script/script.js
@@ -12,6 +12,16 @@ $(document).ready(function () {
     e.preventDefault();
     console.log($(this));
   });
-  let subMenu = document.getElementsByClassName("sub-menu");
+  const subMenu = document.querySelector(".sub-menu");
   console.log(subMenu);
+  const assortment = document.querySelector(".assortment-btn");
+  console.log(assortment);
+  assortment.addEventListener("click", (e) => {
+    console.log("hello");
+    if (subMenu.classList.contains("hide")) {
+      subMenu.classList.remove("hide");
+    } else {
+      subMenu.classList.add("hide");
+    }
+  });
 });
